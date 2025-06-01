967a4808b763d33addb98b6fe64625a8892d4999
Егор Асонов <egorasonovvv@gmail.com>
2025-06-01 17:58:02 +0600

    feat: add infinite scroll

--- src/api/usersRequests.ts
+++ src/api/usersRequests.ts
@@ -1,8 +1,8 @@
 import type { IUser } from "../models/IUser";
 import axios from "./axios";
 
-export async function getUsers(): Promise<IUser[]>{
-    const response = await axios.get('/users');
+export async function getUsers(page: number): Promise<any>{
+    const response = await axios.get('/users', {params: {_page: 1, _per_page: 25}});
     return response.data;
 }
 
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,14 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import {getUsers} from "../../api/usersRequests";
 import './styles.scss';
+import axios from "axios";
 
 type Props = {
     fields: string[];   
 }
 
 const Table = ({fields}: Props) => {
-    const {data} = useQuery({queryKey: ['users'], queryFn: getUsers})
+    const {data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage} = useInfiniteQuery({
+        queryKey: ['users'],
+        queryFn: async ({ pageParam = 1 }) => {
+            const res = await axios.get(`http://localhost:3000/users`, {
+              params: {
+                _page: pageParam,
+                _per_page: 25,
+            }});
+            console.log(res.data.data);
+            return {
+                data: res.data.data,
+                next: pageParam + 1,
+              };
+        },
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => lastPage.next
+    });
     return(
+        
+        <>
         <table className="Table">
             <thead>
                 <tr>
@@ -21,22 +40,35 @@ const Table = ({fields}: Props) => {
             </thead>
             <tbody className="Table_data">
                 {
-                    data?.map(row => {
-                        return(
-                            <tr key={row.id}>
+                    data?.pages?.map(page => {
+                        return page.data.map((item, index) => {
+                            return(
+                                <tr key={index}>
                                 {
-                                    Object.values(row).map(value => {
+                                    Object.values(item).map(value => {
                                         return(
                                             <td key={value}>{value}</td>
                                         )
                                     })
                                 }
-                            </tr>
-                        )
+                                </tr>
+                            )
+                        })
                     })
                 }
             </tbody>
         </table>
+         <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetching}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+              ? 'Load More'
+              : 'Nothing more to load'}
+        </button>
+        </>
     );
 }
 
